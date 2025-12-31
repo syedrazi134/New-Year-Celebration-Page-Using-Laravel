6e5b723a7247242c564d1ba2f9fa4e36f84a5e31
Syed Razi <232697905+syedrazi134@users.noreply.github.com>
2025-12-31 21:02:56 +0500

    Vercel Laravel deployment

--- public/index.php
+++ public/index.php
@@ -18,3 +18,12 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
+
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
+
