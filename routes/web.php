746df6041f5fbbdf08b78b08218e8eee4bdaa090
Syed Razi <232697905+syedrazi134@users.noreply.github.com>
2025-12-31 22:27:39 +0500

    Fix route

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/new-year', function () {
-    return view('new-year');
-});
-
 // Or as the main homepage:
 Route::get('/', function () {
     return view('new-year');
